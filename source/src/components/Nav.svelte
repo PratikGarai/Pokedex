05811f891edb6be54bae0770f392e8d0bcefd89f
PratikGarai <pratikgarai0208@gmail.com>
2021-10-11 11:46:25 +0530

    Navbar with logo

--- source/src/components/Nav.svelte
+++ source/src/components/Nav.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { url } from "@roxi/routify";
     import { user, loginWithGoogle, logout } from "./Auth.svelte";
 
     let _user;
@@ -9,9 +10,14 @@
 <main
     class="w-full flex justify-between items-center bg-red-500 p-4 text-white"
 >
+    <div class="bg-white p-1 rounded-full">
+        <a href={$url("../")}>
+            <img class="w-8 h-8" src="/logo.png" alt="Logo" />
+        </a>
+    </div>
     {#if _user}
         <button on:click={logout}> Logout </button>
     {:else}
-        <button on:click={loginWithGoogle}> Login With Google </button>
+        <button on:click={loginWithGoogle}> Login </button>
     {/if}
 </main>
