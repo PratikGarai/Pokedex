f54d30ded54d5644157bf922986faf510b6f4072
PratikGarai <pratikgarai0208@gmail.com>
2021-10-11 09:42:27 +0530

    Firebase auth setup

--- source/src/auth/index.js
+++ source/src/auth/index.js
@@ -1,8 +1,45 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import { readable } from "svelte/store";
 
 import { firebaseConfig } from "./secrets";
 
 firebase.initializeApp(firebaseConfig);
 
-export const initAuth = () => {}
+const userMapper = claims => ({
+    id : claims.user_id, 
+    name : claims.name, 
+    email : claims.email, 
+    picture : claims.picture
+})
+
+export const initAuth = (useRedirect = false) => {
+    const auth = firebase.auth()
+    const loginWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        if (useRedirect) {
+            return auth.signInWithRedirect(provider)
+        } else {
+            return auth.signInWithPopup(provider)
+        }
+    }
+
+    const user = readable(null, set => {
+        const unsub = auth.onAuthStateChanged(async fireUser => {
+            if (fireUser) {
+                const token = await fireUser.getIdTokenResult()
+                const user = userMapper(token.claims)
+                set(user)
+            } else {
+                set(null)
+            }
+        })
+
+        return unsub
+    })
+
+    return {
+        user, 
+        loginWithGoogle
+    }
+}
